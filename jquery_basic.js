51edd6975279bbeb8c61407d4dc95e8b69a36ad8
Joshua Cole <j_cole_356@yahoo.com>
2015-08-31 13:49:19 -0700

    remove comments from ajax function

--- jquery_basic.js
+++ jquery_basic.js
@@ -38,10 +38,7 @@
         defaults.url += defaults.data;
       }
     }
-
     var request = new XMLHttpRequest();
-    // Looks like there are other ways to do this like on load and
-    // onerror
     request.onreadystatechange = function () {
       if (request.status === 200 && request.readyState === 4) {
         defaults.success(request);
